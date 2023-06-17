e666213631d0db799372bd5c62a5711e595a9f6c
giver-yell <giver.yell@gmail.com>
2023-06-17 08:15:05 +0800

    add: example ferris-says

---
+++ hello-rust/target/debug/deps/hello_rust-cf81c068512b04a1.d
@@ -0,0 +1,5 @@
+/Users/ryoheifujihara/rust-basic/hello-rust/target/debug/deps/hello_rust-cf81c068512b04a1: src/main.rs
+
+/Users/ryoheifujihara/rust-basic/hello-rust/target/debug/deps/hello_rust-cf81c068512b04a1.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/ryoheifujihara/rust-basic/hello-rust/target/debug/hello-rust: /Users/ryoheifujihara/rust-basic/hello-rust/src/main.rs
