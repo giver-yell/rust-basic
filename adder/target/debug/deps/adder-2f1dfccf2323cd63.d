a5210e80502d660e9bcdb9c7ef71febbd9018f46
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-15 08:56:12 +0900

    add: cargo test

---
+++ adder/target/debug/deps/adder-2f1dfccf2323cd63.d
@@ -0,0 +1,5 @@
+/Users/fujiwararyouhei/rust-basic/adder/target/debug/deps/adder-2f1dfccf2323cd63: src/lib.rs
+
+/Users/fujiwararyouhei/rust-basic/adder/target/debug/deps/adder-2f1dfccf2323cd63.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-5144a36f64fecd00.d
@@ -0,0 +1,7 @@
+/Users/fujiwararyouhei/rust-basic/adder/target/debug/deps/adder-5144a36f64fecd00.rmeta: src/lib.rs
+
+/Users/fujiwararyouhei/rust-basic/adder/target/debug/deps/libadder-5144a36f64fecd00.rlib: src/lib.rs
+
+/Users/fujiwararyouhei/rust-basic/adder/target/debug/deps/adder-5144a36f64fecd00.d: src/lib.rs
+
+src/lib.rs:
