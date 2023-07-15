0136b090e313ff257ded2278fd822cebe800405e
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-15 14:19:09 +0900

    add: Custom Failure Messages

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,16 +2,21 @@
 //     left + right
 // }
 
-#[derive(Debug)]
-struct Rectagle {
-    width: u32,
-    height: u32,
-}
+// #[derive(Debug)]
+// struct Rectagle {
+//     width: u32,
+//     height: u32,
+// }
 
-impl Rectagle {
-    fn can_hold(&self, other: &Rectagle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+// impl Rectagle {
+//     fn can_hold(&self, other: &Rectagle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}!", name)
+    String::from("Hello!")
 }
 
 #[cfg(test)]
@@ -29,31 +34,42 @@ mod tests {
     //     panic!("Make this test fail");
     // }
 
+    // #[test]
+    // fn larger_can_hold_smaller() {
+    //     let larger = Rectagle {
+    //         width: 8,
+    //         height: 7,
+    //     };
+    //     let smaller = Rectagle {
+    //         width: 5,
+    //         height: 1,
+    //     };
+
+    //     assert!(larger.can_hold(&smaller));
+
+    //     #[test]
+    //     fn smaller_cannot_hold_larger() {
+    //         let larger = Rectagle {
+    //             width: 8,
+    //             height: 7,
+    //         };
+    //         let smaller = Rectagle {
+    //             width: 5,
+    //             height: 1,
+    //         };
+
+    //         assert!(!smaller.can_hold(&larger));
+    //     }
+    // }
+
+    // Adding Custom Failure Messages
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectagle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectagle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-
-        #[test]
-        fn smaller_cannot_hold_larger() {
-            let larger = Rectagle {
-                width: 8,
-                height: 7,
-            };
-            let smaller = Rectagle {
-                width: 5,
-                height: 1,
-            };
-
-            assert!(!smaller.can_hold(&larger));
-        }
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
