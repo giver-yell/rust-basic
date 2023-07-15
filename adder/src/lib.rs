4693f7e071a4c02c8db0cf524d905f5522e2c902
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-15 15:34:06 +0900

    add: test detail

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -14,9 +14,23 @@
 //     }
 // }
 
-pub fn greeting(name: &str) -> String {
-    // format!("Hello {}!", name)
-    String::from("Hello!")
+// pub fn greeting(name: &str) -> String {
+//     // format!("Hello {}!", name)
+//     String::from("Hello!")
+// }
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -63,13 +77,19 @@ mod tests {
     // }
 
     // Adding Custom Failure Messages
+    // #[test]
+    // fn greeting_contains_name() {
+    //     let result = greeting("Carol");
+    //     assert!(
+    //         result.contains("Carol"),
+    //         "Greeting did not contain name, value was `{}`",
+    //         result
+    //     );
+    // }
+
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        );
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
