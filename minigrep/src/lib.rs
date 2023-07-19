8f90a230085da561886fe719a72e8f868782eb15
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-20 08:19:35 +0900

    add: Writing Code to Pass the Test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -42,6 +42,14 @@ Pick three.";
     }
 }
 
-pub fn search<'a>(query: &str, contents: &str) -> Vec<&'a str> {
-    vec![]
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
