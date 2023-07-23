30c7ab97c53144e4e9efd4caeb7ea4c2c97b0f36
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-24 07:08:33 +0900

    add: Capturing the Environment with Closures

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/Users/fujiwararyouhei/rust-basic/closures/target/debug/closures: /Users/fujiwararyouhei/rust-basic/closures/src/main.rs
---
+++ closures/target/debug/deps/closures-64bafe05d0415a40.d
@@ -0,0 +1,5 @@
+/Users/fujiwararyouhei/rust-basic/closures/target/debug/deps/closures-64bafe05d0415a40: src/main.rs
+
+/Users/fujiwararyouhei/rust-basic/closures/target/debug/deps/closures-64bafe05d0415a40.d: src/main.rs
+
+src/main.rs:
