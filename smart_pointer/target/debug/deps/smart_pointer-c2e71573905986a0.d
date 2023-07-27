f0a7c8fed871106abd794f7be5ccd992bdffe131
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-28 08:21:40 +0900

    add: deref

---
+++ smart_pointer/target/debug/deps/smart_pointer-c2e71573905986a0.d
@@ -0,0 +1,5 @@
+/Users/fujiwararyouhei/rust-basic/smart_pointer/target/debug/deps/smart_pointer-c2e71573905986a0: src/main.rs
+
+/Users/fujiwararyouhei/rust-basic/smart_pointer/target/debug/deps/smart_pointer-c2e71573905986a0.d: src/main.rs
+
+src/main.rs:
---
+++ smart_pointer/target/debug/smart_pointer.d
@@ -0,0 +1,1 @@
+/Users/fujiwararyouhei/rust-basic/smart_pointer/target/debug/smart_pointer: /Users/fujiwararyouhei/rust-basic/smart_pointer/src/main.rs
