b7f1dc2d9a0206f2aa24f0004aa53924aa7a4122
Ryohei Fujihara <giver.yell@gmail.com>
2023-07-29 17:15:39 +0900

    add: drop trait

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,25 +1,48 @@
+// fn main() {
+//     let x = 5;
+//     let y = MyBox::new(x);
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
+
+// struct MyBox<T>(T);
+
+// impl<T> MyBox<T> {
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+
+// use std::ops::Deref;
+
+// impl<T> Deref for MyBox<T> {
+//     type Target = T;
+
+//     fn deref(&self) -> &Self::Target {
+//         &self.0
+//     }
+// }
+
+// Drop
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-}
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-
-use std::ops::Deref;
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
